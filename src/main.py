30b65e435b87f6da875d4d29fb0a7f33e91ba83f
Luis ALberto Bonilla Valencia <luis.bonilla.valencia@gmail.com>
2026-02-25 16:12:14 -0500

    solicita

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# main.py
+
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuál es tu edad? "))
+es_estudiante = True
+
+print("Hola,", nombre)
+print("Edad:", edad)
+print("¿Es estudiante?", es_estudiante)
