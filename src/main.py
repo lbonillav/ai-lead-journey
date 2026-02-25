32d49d936777c9b17f60a54a292d56acfc837db3
Luis ALberto Bonilla Valencia <luis.bonilla.valencia@gmail.com>
2026-02-25 16:39:30 -0500

    Refactorización: separación de responsabilidades en funciones

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 # main.py
+def calcula_edad_en_10_anios(edad):
+    edad_en_10_anios = edad + 10
+    return edad_en_10_anios
 
-nombre = input("¿Cuál es tu nombre? ")
-edad = int(input("¿Cuál es tu edad? "))
-es_estudiante = True
+def obtener_edad():
+    while True:
+        try:
+            edad = int(input("¿Cuál es tu edad? "))
+            break  # Salir del bucle si la entrada es válida
+        except ValueError:
+            print("Error: Por favor, introduce un número válido para la edad.")
 
-print("Hola,", nombre)
-print("Edad:", edad)
-print("¿Es estudiante?", es_estudiante)
+    edad = calcula_edad_en_10_anios(edad)
+    return edad
+
+def main():
+    nombre = input("¿Cuál es tu nombre? ")
+    edad_en_10_anios = obtener_edad()    
+
+    print("Hola,", nombre)
+    print("En 10 años tendrás :", edad_en_10_anios)
+
+if __name__ == "__main__":
+    main()
